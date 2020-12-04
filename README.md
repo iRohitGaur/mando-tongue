c921ef885f819e95ff5954df588743213d82e2e9
Rohit Gaur <rohitgaur7@gmail.com>
2020-12-04 20:58:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![MandoTongue](images/mando-tongue.jpg)
+# mando-tongue
+A translator web app to convert your text from English to Mandalorian Tongue (Mando'a).
+
+### Website: [Mando-Tongue](https://mando-tongue.netlify.app/)
---
+++ app.js
@@ -0,0 +1,48 @@
+var body = document.getElementsByTagName('body')[0];
+var inputText = document.querySelector("#input-text");
+var btnTranslate = document.querySelector("#button-translate");
+btnTranslate.addEventListener("click", translateToMandoa)
+var outputDiv = document.querySelector("#output-translated");
+
+let serverURL = "https://api.funtranslations.com/translate/mandalorian.json"
+
+document.addEventListener('DOMContentLoaded', function() {
+    autosize(document.querySelector('#input-text'));
+}, false);
+
+function translateToMandoa() {
+
+    let textInput = inputText.value;
+
+    if (textInput === "") {
+        alert("Please enter a message first")
+    } else {
+        body.classList.add("running");
+
+        let reqURL = serverURL + "?text=" + textInput
+        
+        fetch(reqURL)
+        .then(function(response) {
+            if (!response.ok) {
+                throw (response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+            body.classList.remove("running");
+        })
+        .catch(errorHandler)
+    }
+}
+
+function errorHandler(error) {
+    body.classList.remove("running");
+    if (error === 429) {
+        alert("Oops! We ran out of coins. Please try again in an hour.");
+    } else if (error == "TypeError: Failed to fetch") {
+        alert("Oops! Failed to fetch - Looks like you're not connected to Internet. Check that please and try again.");
+    } else {
+        alert(error);
+    }
+}
